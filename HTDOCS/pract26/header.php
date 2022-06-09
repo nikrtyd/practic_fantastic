a172c38feb92e6e1733aaf2be4fdda6aad67126d
nikrtyd <nikrtyd@gmail.com>
2022-06-09 13:42:16 +0600

    last commit

--- HTDOCS/pract26/header.php
+++ HTDOCS/pract26/header.php
@@ -2,9 +2,9 @@
   <div class="popup">
     <a href="index.php">Главная</a>
     <?php if ($user_id) : ?>
-      <a id="show_add_photo" href="#">Фото</a>
+      <a id="show_add_photo" href="#">Добавить фото</a>
     <?php endif; ?>
-    <a href="#">Посты</a>
+    <a href="#">Ваши посты</a>
     <a href="user.php">Личный кабинет</a>
     <?php if ($user_id) : ?>
       <a href="logout.php">Выйти</a>
